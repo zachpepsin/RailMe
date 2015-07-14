969a1e71e46422163bb72101b411d50b87eb005c
Zach Pepsin <zachpepsin@gmail.com>
2015-07-14 13:30:24 -0400

    made riverlink jsons, fix BOM bug attempt

--- prod/csvToJson.ps1
+++ prod/csvToJson.ps1
@@ -154,7 +154,8 @@ foreach ($file in $fileNames){
 	<# $fout = $fin | ConvertTo-Json #> #only works in PowerShell v3+, have to use function
 	$fout = $fin | ConvertTo-JSON  #convert object to JSON format
 	Write-Host "Writing to .\$agency\$folder\$file.json..."
-	$fout > ".\$agency\$folder\$file.json"
+	#$fout > ".\$agency\$folder\$file.json"
+	[System.IO.File]::WriteAllLines(".\$agency\$folder\$file.json", $fout)
 	Write-Host "Done!`n"
 }
 
